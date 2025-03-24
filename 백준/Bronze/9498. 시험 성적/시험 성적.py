43cc08e9adf39895fe0e7771cfd874a0d226c46c
Sora <140125644+leeesoraaa@users.noreply.github.com>
2025-03-24 17:04:47 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,14 @@
+score = int(input())
+def abcdf(score):
+    if 90 <= score <=100:
+        return 'A'
+    elif 80 <= score <= 89:
+        return 'B'
+    elif 70 <= score <= 79:
+        return 'C'
+    elif 60 <= score <= 69:
+        return 'D'
+    else:
+        return 'F'
+    
+print(abcdf(score))
