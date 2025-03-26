93eb8ba5a92ab80ffc7136874168729d82f706ff
Sora <140125644+leeesoraaa@users.noreply.github.com>
2025-03-26 14:43:21 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,6 +1,7 @@
-import string
-s = str(input())
-alphabet = list(string.ascii_lowercase)
-for alpha in alphabet:
-    count = s.index(alpha) if alpha in s else -1
-    print(count, end=' ')
+a = [-1] * 26
+s = input()
+for i in s:
+    t = ord(i) - 97 # ord(i): 아스키코드 정수
+    if a[t] == -1: # a[t]: 해당 알파벳의 인덱스 위치
+        a[t] = s.index(i)
+print(*a) # a의 원소를 공백으로 구분해 한 줄에 출력
