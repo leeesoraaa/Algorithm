9ade53899cfbaaba4956a29cfa66fa78ff9a7e8c
Sora <140125644+leeesoraaa@users.noreply.github.com>
2025-03-27 16:05:09 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,8 +1,9 @@
 n = int(input())
 string = input()
 total = 0
+M = 1234567891
 for idx, s in enumerate(string):
-    hash_str = (ord(s)-96)*31**idx
-    total+=hash_str
+    hash_str = (ord(s)-96)*(31**idx%M)
+    total=(total + hash_str) % M
 
 print(total)
