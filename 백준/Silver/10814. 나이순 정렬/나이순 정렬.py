e2ca5e43222a8138da07149792f558c0f51647af
Sora <140125644+leeesoraaa@users.noreply.github.com>
2025-04-07 18:46:44 +0900

    [Silver V] Title: 나이순 정렬, Time: 184 ms, Memory: 46020 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -2,10 +2,10 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 people = []
-for i in range(n):
+for _ in range(n):
     age, name = input().split()
-    people.append((int(age), name, i))
-people.sort(key=lambda x : (x[0], x[2]))
+    people.append((int(age), name))
+people.sort(key=lambda x : x[0])
 
 for p in people:
     print(f'{p[0]} {p[1]}')
